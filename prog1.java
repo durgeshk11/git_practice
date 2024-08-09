324aaf63d174d8561592b7aa7341f452888629da
AmeyKhandekar1 <85950284+AmeyKhandekar1@users.noreply.github.com>
2024-08-09 22:48:06 +0530

    Update prog1.java
    modified program1.java 1st  printing line  by ak

--- prog1.java
+++ prog1.java
@@ -1,6 +1,6 @@
 class Prog1{
  public static void main(String args[]){
-   System.out.println("***Printing line from prog1 by alen****");
+   System.out.println("***Printing line from prog1 by ak****");
    System.out.println("writing second line by alen");
  }
 }
