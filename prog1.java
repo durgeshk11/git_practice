d36aeb007e3bcad538f9d805b5e7ee484ecbcaa1
ak <ak@gmail.com>
2024-08-09 22:27:59 +0530

    prog1 added by owner

---
+++ prog1.java
@@ -0,0 +1,5 @@
+class Prog1{
+ public static void main(String args[]){
+   System.out.println("***Printing line from prog1****");
+ }
+}[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C pusshed by alen");[B[B[B[B
